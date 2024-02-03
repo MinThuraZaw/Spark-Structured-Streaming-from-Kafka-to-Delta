5a7380c855b2f79f4ed475fa3df4a478d3e42ad5
Min Thura Zaw <minthurazaw619@gmail.com>
2024-02-03 11:05:59 +0700

    Update structured-streaming-kinesis.scala

--- databricks/structured-streaming-kinesis.scala
+++ databricks/structured-streaming-kinesis.scala
@@ -4,8 +4,31 @@ import org.apache.spark.sql.functions._
 
 // COMMAND ----------
 
+id"),
+    col("dtm").alias("timestamp"),
+    col("tz").alias("device_timezone"),
+    col("res").alias("device_screenresolution"),
+    col("uid").alias("user_id"),
+    col("p").alias("app_platform"),
+    col("se_ac").alias("event_id"),
+    col("se_pr").alias("property"),
+    col("se_va").alias("value")
+
+// COMMAND ----------
+
 // MAGIC %sql
-// MAGIC -- create or replace table 
+// MAGIC create or replace table demo_catalog.demo_schema.kinesis_raw_events 
+// MAGIC (
+// MAGIC   id string,
+// MAGIC   timestamp string,
+// MAGIC   device_timezone string,
+// MAGIC   device_screenresolution string,
+// MAGIC   user_id string,
+// MAGIC   app_platform string,
+// MAGIC   event_type string,
+// MAGIC   property string,
+// MAGIC   value string
+// MAGIC )
 
 // COMMAND ----------
 
@@ -55,6 +78,32 @@ val df = df_col.withColumn("extract_data", explode(col("data"))).select("extract
 // COMMAND ----------
 
 display(df.limit(20))
+
+// COMMAND ----------
+
+val df_table = df.select(
+    col("eid").alias("id"),
+    col("dtm").alias("timestamp"),
+    col("tz").alias("device_timezone"),
+    col("res").alias("device_screenresolution"),
+    col("uid").alias("user_id"),
+    col("p").alias("app_platform"),
+    col("se_ac").alias("event_type"),
+    col("se_pr").alias("property"),
+    col("se_va").alias("value")
+)
+
+// COMMAND ----------
+
+display(df_table.limit(20))
+
+// COMMAND ----------
+
+df_table
+    .writeStream.format("delta")
+    .outputMode("append")
+    .option("checkpointLocation","demo_catalog.demo_schema.kinesis_raw_events")
+    .table("demo_catalog.demo_schema.kinesis_raw_events")
 
 // COMMAND ----------
 
